a0b928abb78db35a7f63e13fbf2de52310a0f4b8
Dhruv Maheshwari <mrdm026@gmail.com>
2022-01-28 18:13:52 +0000

    print short title, author and quantity

---
+++ challenges/string_function_challenges/challenge_7.sql
@@ -0,0 +1,7 @@
+# print short title, author and quantity
+
+SELECT
+    CONCAT(SUBSTRING(title, 1, 10), "...") AS 'short title',
+    CONCAT(author_lname, ',', author_fname) AS 'author',
+    CONCAT(stock_quantity, ' in stock') AS 'quantity'
+FROM books;
