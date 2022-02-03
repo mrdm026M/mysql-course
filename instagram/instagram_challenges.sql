eb94f76e3eaf00d94f1dd2b3b918620acbd36e06
Dhruv Maheshwari <mrdm026@gmail.com>
2022-02-03 03:51:00 +0000

    Instagram Clone Database Challenges complete

--- instagram/instagram_challenges.sql
+++ instagram/instagram_challenges.sql
@@ -5,10 +5,62 @@
 # LIMIT 5;
 
 # Challenge 2 - most popular registration date
-SELECT
-    DAYNAME(created_at) AS day,
-    COUNT(*) AS total
-FROM users
-GROUP BY day
-ORDER BY total DESC
-LIMIT 2;
+# SELECT
+#     DAYNAME(created_at) AS day,
+#     COUNT(*) AS total
+# FROM users
+# GROUP BY day
+# ORDER BY total DESC
+# LIMIT 2;
+
+# Challenge 3 - find inactive user who have never posted a photo
+# SELECT
+#     username
+# FROM users
+# LEFT JOIN photos
+#     ON users.id = photos.user_id
+# WHERE photos.id IS NULL;
+
+# Challenge 4 - Identify most popular photo (and user who created it)
+# SELECT 
+#     username,
+#     photos.id,
+#     photos.image_url, 
+#     COUNT(*) AS total
+# FROM photos
+# INNER JOIN likes
+#     ON likes.photo_id = photos.id
+# INNER JOIN users
+#     ON photos.user_id = users.id
+# GROUP BY photos.id
+# ORDER BY total DESC
+# LIMIT 1;
+
+# Challenge 5 - calculate avg no. of photos per user
+# SELECT (
+#     SELECT Count(*) 
+#     FROM photos) / (
+#         SELECT Count(*) 
+#         FROM users) AS avg;
+
+# Challenge 6 - Find the five most popular hashtags
+
+# SELECT tags.tag_name, 
+#        Count(*) AS total 
+# FROM   photo_tags 
+#        JOIN tags 
+#          ON photo_tags.tag_id = tags.id 
+# GROUP  BY tags.id 
+# ORDER  BY total DESC 
+# LIMIT  5; 
+
+# Challenge 6 - Finding the bots - the users who have liked every single photo
+
+SELECT username, 
+       Count(*) AS num_likes 
+FROM users 
+       INNER JOIN likes 
+               ON users.id = likes.user_id 
+GROUP BY likes.user_id 
+HAVING num_likes = (SELECT Count(*) 
+                    FROM   photos); 
